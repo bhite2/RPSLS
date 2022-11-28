38c51c8327aebf4b96ba760af4035215eb5d9631
Bryan Hite <bhite2@mail.usf.edu>
2022-11-27 21:22:56 -0500

    Created game.py module. Created welcome function.

---
+++ game.py
@@ -0,0 +1,25 @@
+
+
+
+
+class Game():
+
+    def welcome(self):
+        print('Welcome to a wonderful game of chance!')
+        print('Each player picks a gesture and reveals it at the same time. Player with the winning gesture wins the round.')
+        print('If both players use the same gesture, that is a tie. The round will be replayed.')
+        print('First player to get two wins is crowned the winner.')
+        print('You can choose between Rock, Paper, Scissors, Lizard or Spock')
+        print('Rock crushes Scissors')
+        print('Scissors cuts Paper')
+        print('Paper covers Rock')
+        print('Rock crushes Lizard')
+        print('Lizard poisons Spock')
+        print('Spock smashes Scissors')
+        print('Scissors decapitates Lizard')
+        print('Lizard eats Paper')
+        print('Paper disproves Spock')
+        print('Spock vaporizes Rock')
+        print('May the best player win')
+
+    
